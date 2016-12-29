c359fc3bee349681040370de5d53e533c889162a
Mark Woods <markstevenwoods@gmail.com>
2016-12-29 15:19:38 -0600

    adding apiai commands

--- api.py
+++ api.py
@@ -12,53 +12,64 @@ basic_auth = BasicAuth(app)
 
 api = Api(app)
 
-pilsners = []
-stouts = []
-ipas = []
-dipas = []
 beers = []
 
 def addBeer(style, brewer, product):
-    style.append({"brewer": brewer, "product": product})
-    beers.append({"brewer": brewer, "product": product})
+    beers.append({"brewer": brewer, "product": product, "style": style})
     
-addBeer(pilsners, "Pilsner Urquell", "Pilsner Urquell")
-addBeer(pilsners, "AB Inbev", "Stella Artois")
-addBeer(pilsners, "Stiegl", "Pils")
-addBeer(pilsners, "Trummer", "Pils")
+addBeer("Pilsner", "Pilsner Urquell", "Pilsner Urquell")
+addBeer("Pilsner", "AB Inbev", "Stella Artois")
+addBeer("Pilsner", "Stiegl", "Pils")
+addBeer("Pilsner", "Trummer", "Pils")
+addBeer("Pilsner", "Two Roads", "Ol' Factory Pils")
 
-addBeer(stouts, "Victory", "Donnybrook Stout")
-addBeer(stouts, "Deschutes", "Obsidian Stout")
-addBeer(stouts, "Brooklyn", "Black Chocolate Stout")
+addBeer("Stout", "Victory", "Donnybrook Stout")
+addBeer("Stout", "Deschutes", "Obsidian Stout")
+addBeer("Stout", "Brooklyn", "Black Chocolate Stout")
+addBeer("Stout", "4 Hands", "Chocolate Milk Stout")
 
-addBeer(ipas, "Founders", "All Day IPA")
-addBeer(ipas, "Bells", "Two Hearted")
-addBeer(ipas, "Temperance", "Gatecrasher")
+addBeer("IPA", "Founders", "All Day IPA")
+addBeer("IPA", "Two Roads", "Lil Heaven")
+addBeer("IPA", "Bells", "Two Hearted")
+addBeer("IPA", "Temperance", "Gatecrasher")
 
-addBeer(dipas, "Two Roads", "Road 2 Ruin")
-addBeer(dipas, "The Alchemist", "Heady Topper")
-addBeer(dipas, "Russian River", "Pliny the Elder")
-addBeer(dipas, "Lagunitas", "Hop Stoopid")
+addBeer("Double IPA", "Two Roads", "Road 2 Ruin")
+addBeer("Double IPA", "The Alchemist", "Heady Topper")
+addBeer("Double IPA", "Russian River", "Pliny the Elder")
+addBeer("Double IPA", "Lagunitas", "Hop Stoopid")
 
 class Pilsners(Resource):
     def get(self):
-        return pilsners
+        return [b for b in beers if b["style"] == "Pilsner"]
         
 class Stouts(Resource):
     def get(self):
-        return stouts
+        return [b for b in beers if b["style"] == "Stout"]
         
 class IPAs(Resource):
     def get(self):
-        return ipas
+        return [b for b in beers if b["style"] == "IPA"]
         
 class Dipas(Resource):
     def get(self):
-        return dipas
+        return [b for b in beers if b["style"] == "Double IPA"]
         
 class Beers(Resource):
     def get(self):        
         return beers
+        
+class Brewers(Resource):
+    def get(self):
+        return [b["brewer"] for b in beers]
+        
+class Styles(Resource):
+    def get(self):
+        return list(set([b["style"] for b in beers]))   # set selects only unique values
+    
+class Names(Resource):
+    def get(self):
+        return [b["product"] for b in beers]
+        
 
 # Pure rest
 api.add_resource(Pilsners, '/pilsners')
@@ -66,31 +77,42 @@ api.add_resource(Stouts, '/stouts')
 api.add_resource(IPAs, '/ipas')
 api.add_resource(Dipas, '/dipas')
 api.add_resource(Beers, '/beers')
+api.add_resource(Brewers, '/brewers')
+api.add_resource(Styles, '/styles')
+api.add_resource(Names, '/names')
 
 # Rest, but adhering to format required for API.ai
 class Apiai(Resource):
-    def post(self):  
-        
-        parser = reqparse.RequestParser()
-        parser.add_argument('action', location='json')
-        args = parser.parse_args() 
-        #print 'Requested action: ' + args['action']  
+    def post(self):          
         payload = request.get_json()
         print 'action: ' + payload['result']['action']
-        
-        speech = 'On tap I have '
-        for beer in beers:
-            speech += beer['brewer'] + ' ' + beer['product'] + ', '
+
+        if payload['result']['action'] == 'Beer.List':
+            speech = 'On tap I have '
+            for beer in beers[:-1]:
+                speech += beer['brewer'] + ' ' + beer['product'] + ', '
+            speech += 'and ' + beer['brewer'] + ' ' + beer['product'] + '. '   # pretty up the last one
+            speech += 'Now, what can I get you?'                            # Webhook m/provide all text
             
-        # remove the trailing comma
-        speech = speech[:-2]
-        return {
-            "speech": speech,
-            "displayText": speech,
-            # "data": data,
-            # "contextOut": [],
-            "source": "bartender service"
-        }
+            return {
+                "speech": speech,
+                "displayText": speech,
+                # "data": data,
+                # "contextOut": [],
+                "source": "bartender service"
+            }
+
+        if payload['result']['action'] == 'Beer.Order':
+            print payload['result']['parameters']
+
+            speech = 'I will get you a beer.'
+            return {
+                "speech": speech,
+                "displayText": speech,
+                # "data": data,
+                # "contextOut": [],
+                "source": "bartender service"
+            }
 
 
 api.add_resource(Apiai, '/apiai')
